32e01fdb20400452906458b734c2b17bd1903bba
vs-wilson <739929382@qq.com>
2020-03-13 15:17:47 +0800

    first commit

---
+++ src/test/java/com/ittg/ttgconfigserver/TtgConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ittg.ttgconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtgConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
